1c8e613fe2ad51d172ce72922b12867102039993
eortiz-30 <eortiz30@stuy.edu>
2021-10-01 09:59:29 -0400

    init setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,24 @@
+/* Emily Ortiz
+APCS
+HW08 -- Refactor Freshie Zero
+2021-09-31
+
+DISCOVERIES
+- Delete repo on my computer and clone again to resolve merging errors
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class Greet{
+	public static void main(String[] args){
+		greet("Gabriel");
+		greet("Emily");
+		greet("Jonathan");
+	}
+
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
+
