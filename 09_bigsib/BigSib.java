67c4edef5c2d739351f18dfa0248ef4b29b64fb2
eortiz-30 <eortiz30@stuy.edu>
2021-10-03 22:59:03 -0400

    init setup

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,23 @@
+/* Baked Potatoes: Emily Ortiz + Applesauce, Jacob Ng + Preguac, Vansh Saboo + Tiffany
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-04
+
+DISCOVERIES
+- Greet runs the greet function in BigSib
+
+UNRESOLVED QUESTIONS
+- I compiled both Greet.java and BigSib.java. Will Greet run without compliling BigSib?
+- Does BigSib have to be in the same directory as Greet for Greet to run?
+
+*/
+
+public class BigSib{
+	public static void main(String[] args){
+
+	}
+
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,21 @@
+/* Baked Potatoes: Emily Ortiz + Applesauce, Jacob Ng + Preguac, Vansh Saboo + Tiffany
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-04
+
+DISCOVERIES
+- Greet runs the greet function in BigSib
+
+UNRESOLVED QUESTIONS
+- I compiled both Greet.java and BigSib.java. Will Greet run without compliling BigSib?
+- Does BigSib have to be in the same directory as Greet for Greet to run?
+
+*/
+
+public class Greet{
+	public static void main(String[] args){
+		BigSib.greet("Flim");
+		BigSib.greet("Flam");
+		BigSib.greet("Kazaam");
+	}
+}
