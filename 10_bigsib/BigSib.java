ac6b2a2b6ad6703d9928e046b4d0a3801b44f3f7
eortiz-30 <eortiz30@stuy.edu>
2021-10-04 23:36:14 -0400

    init setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/* Baked Potatoes: Emily Ortiz + Applesauce, Jacob Ng + Preguac, Vansh Saboo + Tiffany
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-05 (due date)
+
+DISCOVERIES
+- Had to change greet in BigSib.java from void to String
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class BigSib{
+	public static void main(String[] args){
+
+	}
+
+	public static String greet(String name) {
+		return("Why, hello there, " + name + ". How do you do?");
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/* Baked Potatoes: Emily Ortiz + Applesauce, Jacob Ng + Preguac, Vansh Saboo + Tiffany
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-05 (due date)
+
+DISCOVERIES
+- Had to change greet in BigSib.java from void to String
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class Greet{
+	public static void main(String[] args){
+		System.out.println( BigSib.greet("Crosby") );
+		System.out.println( BigSib.greet("Stills") );
+		System.out.println( BigSib.greet("Nash") );
+	}
+}
