7d559ae8311251316fc6b19b42a300e12b49371b
Emily Ortiz <78173159+eortiz-30@users.noreply.github.com>
2021-10-06 12:00:01 -0400

    Update BigSib.java
    added it online b/c issues commiting through GitBash

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,13 +1,34 @@
+/* Emily Ortiz, Jason
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-06 (due date)
+
+DISCO
+- static allows a variable to be used between methods
+
+QCC
+- Spent over 30 min on the assignment
+- Need to review this assignment and see the solution
+- What is an instance variable?
+- How do I get other greetings to display? (e.g. line 22)
+- Were we allowed to edit the given Greet.java code?
+
+*/
+
 public class BigSib{
 	public static void main(String[] args){
-
+		setHelloMsg("Salutations");
+		System.out.println( greet("Dr. Spaceman") );
+		// Doesn't print "Salutations Dr. Spaceman when Greet.java is run"
 	}
+	static String HelloMsg;
 
-  public static void setHelloMsg(String message){
-    String HelloMsg = message;
+	public static String setHelloMsg(String message){
+		HelloMsg = message;
+		return HelloMsg;
   }
 	public static String greet(String name) {
-		return( "" + name);
+		return(HelloMsg + " " + name);
 	}
 
 
