938a10ac7a2d18bd5a541984f2d887252e35bb93
Emily Ortiz <78173159+eortiz-30@users.noreply.github.com>
2021-10-06 12:00:55 -0400

    Update Greet.java
    added online b/c issues committing through GitBash

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -4,11 +4,14 @@ HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 2021-10-06 (due date)
 
 DISCO
+- static allows a variable to be used between methods
 
 QCC
 - Spent over 30 min on the assignment
+- Need to review this assignment and see the solution
 - What is an instance variable?
-- How do I use a variable between methods?
+- How do I get other greetings to display?
+- Were we allowed to edit the given Greet.java code?
 
 */
 
@@ -24,3 +27,4 @@ public class Greet {
 
   } //end main()
 } //end Greet
+
