6b7afbdb9d35ab94cca1e3369a27cab6aa89b993
eortiz-30 <eortiz30@stuy.edu>
2021-10-07 22:44:44 -0400

    init setup

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,30 @@
+/*
+TNPG: ziggy's fishball amoebaly rangers (Emily Ortiz - Applesauce, Ziying Jian - Scruffy, Nina Jiang - Miss Alpaca, Faiza Huda - Truthful Tom)
+APCS
+HW 14 -- Customize Your Creation
+2021-10-7
+*/
+
+public class Greet {
+  public static void main (String[] args){
+	String greeting;
+
+	BigSib richard = new BigSib("Word up");
+  BigSib grizz = new BigSib("Hey ya");
+  BigSib dotCom = new BigSib("Sup");
+  BigSib tracy = new BigSib("Salutations");
+
+  greeting = richard.greet("freshman");
+	System.out.println(greeting);
+
+	greeting = tracy.greet("Dr.Spaceman");
+	System.out.println(greeting);
+
+	greeting = grizz.greet("Kong Foey");
+	System.out.println(greeting);
+
+	greeting = dotCom.greet("mom");
+	System.out.println(greeting);
+	}
+
+}
