bdc03d4607985bd095a7d1c361bc0a99018cef0b
eortiz-30 <eortiz30@stuy.edu>
2021-12-02 14:02:49 -0500

    hw42

---
+++ 42_rat/Rational.java
@@ -0,0 +1,130 @@
+/*****************************************************
+ * Perfect Purple Petunias (John Gupta-She, Lauren Lee, Emily Ortiz)
+ * APCS pd08
+ * HW41 -- Be Rational
+ * 2021-12-1
+ * time spent: 0.5 hrs
+ *****************************************************/
+
+ /*****************************************************
+DISCO:
+* default is a key word
+* double is the most precise floating point primitive
+QCC:
+* why does the toString() method have to be public?
+  *****************************************************/
+
+public class Rational{
+
+  private int numerator;
+  private int denominator;
+
+  public Rational(){
+    numerator = 0;
+    denominator = 1;
+  }
+
+  public Rational(int a, int b){
+    if (b == 0){
+      numerator = 0;
+      denominator = 1;
+      System.out.println("Denominator can't be 0. Rational set to 0/1.");
+    }else{
+      numerator = a;
+      denominator = b;
+    }
+  }
+
+  public String toString(){
+    return numerator + "/" + denominator;
+  }
+
+  private double floatValue(){
+    return (double)numerator/ (double) denominator;
+  }
+
+  private void multiply(Rational a){
+    numerator = numerator * a.numerator;
+    denominator = denominator * a.denominator;
+  }
+
+  private void divide(Rational a){
+    if (a.numerator != 0){
+      numerator = numerator * a.denominator;
+      denominator = denominator * a.numerator;
+    } else {
+      System.out.println("Cannot divide by 0.");
+    }
+  }
+
+  private void add(Rational a){
+    numerator = numerator * a.denominator + a.numerator * denominator;
+    denominator = denominator * a.denominator;
+  }
+
+  private void subtract(Rational a){
+    numerator = numerator * a.denominator - a.numerator * denominator;
+    denominator = denominator * a.denominator;
+  }
+
+  private int gcd(){
+    int i = 1;
+    int currentGCD = 1;
+    int lesserNum = numerator;
+    if (numerator > denominator) {
+        lesserNum = denominator;
+    }
+    while (i <= lesserNum){
+        if ( (numerator % i == 0) && (denominator % i == 0)){
+            currentGCD = i;
+        }
+        i++;
+    }
+    if (numerator == denominator) {
+        currentGCD = numerator;
+    } return currentGCD;
+  }
+
+  public static void main(String[] args){
+    System.out.println();
+
+    Rational def = new Rational();
+    System.out.println("Default contructor: " + def + " ...should be 0/1");
+    System.out.println("//====================================");
+
+    Rational d0 = new Rational(1, 0);
+    System.out.println("Denominator set to 0: " + d0 + " ...should be 0/1");
+    System.out.println("//====================================");
+
+    Rational r = new Rational(1,8);
+    Rational s = new Rational(1,2);
+    System.out.println("r: " + r + " ...should be 1/8");
+    System.out.println("s: " + s + " ...should be 1/2");
+    System.out.println("float value of r " + r.floatValue() + " ...should be 0.125");
+    System.out.println("float value of s " + s.floatValue() + " ...should be 0.5");
+    r.multiply(s);
+    System.out.println("value of r after multiplication: " + r + " ...1/16");
+    System.out.println("value of s after multiplication: " + s + " ...1/2");
+    r.divide(s);
+    System.out.println("value of r after division: " + r + " ...2/16");
+    System.out.println("value of s after division: " + s + " ...1/2");
+    r.add(s);
+    System.out.println("value of r after addition: " + r + "...20/32");
+    System.out.println("value of s after addition: " + s + "...1/3");
+
+/**
+    System.out.println("//====================================");
+    Rational r3 = new Rational(2, 3);
+    Rational r4 = new Rational();
+
+    System.out.println(r3);
+    System.out.println(r4);
+    System.out.println(r3.floatValue());
+    r3.multiply(r4);
+    System.out.println(r3);
+    r3.divide(r4);
+    System.out.println(r3);
+    **/
+  }
+
+}
