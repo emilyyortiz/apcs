2c7a11495af0bd9bcf90b985e7859c52af736e34
Emily Ortiz <78173159+eortiz-30@users.noreply.github.com>
2021-12-01 20:25:45 -0500

    hw41 update

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -1,4 +1,21 @@
+/*****************************************************
+ * Perfect Purple Petunias (John Gupta-She, Lauren Lee, Emily Ortiz)
+ * APCS pd08
+ * HW41 -- Be Rational
+ * 2021-12-1
+ * time spent: 0.5 hrs
+ *****************************************************/
+
+ /*****************************************************
+DISCO:
+* default is a key word
+* double is the most precise floating point primitive
+QCC:
+* why does the toString() method have to be public?
+  *****************************************************/
+
 public class Rational{
+
   private int numerator;
   private int denominator;
 
@@ -8,11 +25,12 @@ public class Rational{
   }
 
   public Rational(int a, int b){
-    numerator = a;
     if (b == 0){
+      numerator = 0;
       denominator = 1;
-      System.out.println("Denominator can't be 0. Denominator set to 1.");
+      System.out.println("Denominator can't be 0. Rational set to 0/1.");
     }else{
+      numerator = a;
       denominator = b;
     }
   }
@@ -21,8 +39,8 @@ public class Rational{
     return numerator + "/" + denominator;
   }
 
-  private float floatValue(){
-    return (float)numerator/ (float) denominator;
+  private double floatValue(){
+    return (double)numerator/ (double) denominator;
   }
 
   private void multiply(Rational a){
@@ -35,5 +53,43 @@ public class Rational{
     denominator = denominator * a.numerator;
   }
 
+  public static void main(String[] args){
+    System.out.println();
+
+    Rational def = new Rational();
+    System.out.println("Default contructor: " + def + " ...should be 0/1");
+    System.out.println("//====================================");
+
+    Rational d0 = new Rational(1, 0);
+    System.out.println("Denominator set to 0: " + d0 + " ...should be 0/1");
+    System.out.println("//====================================");
+
+    Rational r = new Rational(1,8);
+    Rational s = new Rational(1,2);
+    System.out.println("r: " + r + " ...should be 1/8");
+    System.out.println("s: " + s + " ...should be 1/2");
+    System.out.println("float value of r " + r.floatValue() + " ...should be 0.125");
+    System.out.println("float value of s " + s.floatValue() + " ...should be 0.5");
+    r.multiply(s);
+    System.out.println("value of r after multiplication: " + r + " ...1/16");
+    System.out.println("value of s after multiplication: " + s + " ...1/2");
+    r.divide(s);
+    System.out.println("value of r after division: " + r + " ...2/16");
+    System.out.println("value of s after division: " + s + " ...1/2");
+
+/**
+    System.out.println("//====================================");
+    Rational r3 = new Rational(2, 3);
+    Rational r4 = new Rational();
+
+    System.out.println(r3);
+    System.out.println(r4);
+    System.out.println(r3.floatValue());
+    r3.multiply(r4);
+    System.out.println(r3);
+    r3.divide(r4);
+    System.out.println(r3);
+    **/
+  }
 
 }
