760ef422226617c3795abc9a8cb4f5c86f969f31
eortiz-30 <eortiz30@stuy.edu>
2022-03-11 12:34:06 -0500

    Linked List setup

---
+++ 75_/List.java
@@ -0,0 +1,18 @@
+// writing interface
+
+public interface List{
+  public boolean add( String x ); // prob add element at end
+  public String get( int i ); // strings at diff indeces
+  public String set( int i, String x );
+  public int size();
+}
+
+/*
+Also need:
+Cons cell class/ NODE
+
+Node Properties:
+- CAR
+- CDR
+  - be a Node or nill
+*/
